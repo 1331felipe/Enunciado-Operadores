bac0a5bf76e2feb2a9da38820ed3936cbe8cc288
1331felipe <felipehuh2023@gmail.com>
2023-04-28 16:39:28 -0300

    Enunciado-Operadores

---
+++ index.js
@@ -0,0 +1,45 @@
+//Exercicios de interpretação de código.
+//1)
+//a) false
+//b) false
+//c) true
+//d) boolean
+
+//2)/3) the type "Number()" must be claimed, otherwise the numbers will be read as Strings.
+
+//Exercicios de Escrita de código.
+//1) 
+/*let yourAge = Number(prompt ("How old are you?"))
+let bestFriendAge = Number(prompt ("How old are you best friend?"))
+
+console.log("Are you older than your best friend?", yourAge>bestFriendAge)
+console.log("The age difference between you guys is=", yourAge-bestFriendAge)*/
+
+//2)
+
+//let number = prompt("type any even number.")
+//console.log (number%2)
+
+/*c)/d) The percentage of any EVEN number (multiplied by 2?) will result as a ZERO.
+Differently, the percentage of any ODD number (multiplied by 2?) will result as a ONE.*/
+
+//3)
+
+/*let yourAge = Number(prompt("How old are you?"))
+
+console.log("you are",yourAge*12, "months old.")
+console.log("you are",yourAge*365, "days old.")
+console.log("you are",yourAge*8760, "hours old.")*/
+
+// let firstNumber = Number(prompt("Type any number."))
+// let secondNumber = Number(prompt("Type another number."))
+
+// console.log("Is the first number greater than the second?",firstNumber>secondNumber)
+// console.log("Is the first number equal to the second?", firstNumber===secondNumber)
+// console.log("Is the first number divisble by the second?", firstNumber%secondNumber===0)
+// console.log("Is the second number divisble by the first?",secondNumber%firstNumber===0 )
+        
+
+
+
+
